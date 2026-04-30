465c901dc4bc0bb46b84a42cf52f9383d1c952e3
LibRComm <agape@ikmail.com>
2026-04-30 18:52:20 +0200

    Update About.tsx

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -7,18 +7,19 @@ import {
 const About: React.FC = () => {
   return (
     <AboutWrapper data-testid="about">
-      <p>
-        Hi, my name is <HighlightSpan>Sat Naing</HighlightSpan>!
-      </p>
-      <p>
-        I'm <HighlightAlt>a full-stack developer</HighlightAlt> based in Yangon,
-        Myanmar.
-      </p>
-      <p>
-        I am passionate about writing codes and <br />
-        developing web applications to solve real-life challenges.
-      </p>
-    </AboutWrapper>
+  <p>
+    Bonjour, je m'appelle <HighlightSpan>Harold</HighlightSpan>.
+  </p>
+  <p>
+    Passionnée par l’informatique, j’ai choisi de me réorienter vers ce domaine en intégrant une formation de Technicienne Supérieure Systèmes et Réseaux, <br />
+    que j’ai ensuite complétée par celle d'Administratrice d’Infrastructures Sécurisées.
+    <br /><br />
+    Au cours de mon parcours, j’ai développé des compétences en gestion des systèmes, administration réseau et sécurisation des infrastructures.
+    <br /><br />
+    Je suis actuellement à la recherche d’un poste de Technicienne Informatique ou d’Administratrice Systèmes et Réseaux junior, <br />
+    afin de mettre en pratique mes compétences et continuer à évoluer dans un environnement technique stimulant.
+  </p>
+</AboutWrapper>
   );
 };
 
