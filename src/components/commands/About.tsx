aeed3712b8e62c60b8106d023e9b185cf2984a40
LibRComm <agape@ikmail.com>
2026-04-30 22:40:02 +0200

    Update About.tsx

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -14,9 +14,9 @@ const About: React.FC = () => {
     Passionnée par l’informatique, j’ai choisi de me réorienter vers ce domaine en intégrant une formation de Technicienne Supérieure Systèmes et Réseaux, <br />
     que j’ai ensuite complétée par celle d'Administratrice d’Infrastructures Sécurisées.
     <br /><br />
-    Au cours de mon parcours, j’ai développé des compétences en gestion des systèmes, administration réseau et sécurisation des infrastructures.
+    Au cours de mon parcours, j’ai développé des compétences en administration des systèmes, réseaux et sécurisation des infrastructures.
     <br /><br />
-    Je suis actuellement à la recherche d’un poste de Technicienne Informatique ou d’Administratrice Systèmes et Réseaux junior, <br />
+    Je suis actuellement à la recherche d’un poste de Technicienne Informatique ou d’Administratrice d'Infrastructures sécurosées junior, <br />
     afin de mettre en pratique mes compétences et continuer à évoluer dans un environnement technique stimulant.
   </p>
 </AboutWrapper>
