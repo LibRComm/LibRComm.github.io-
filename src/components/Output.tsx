7b1a8ac2850f81ab6cb304ee12f083c07b60e1ab
LibRComm <agape@ikmail.com>
2026-04-30 20:08:15 +0200

    Update Output.tsx

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -9,7 +9,8 @@ import Help from "./commands/Help";
 import Welcome from "./commands/Welcome";
 import History from "./commands/History";
 import Projects from "./commands/Projects";
-import Socials from "./commands/Socials";
+import Skills from "./commands/Skills";
+import Experience from "./commands/Experience";
 import Themes from "./commands/Themes";
 import { OutputContainer, UsageDiv } from "./styles/Output.styled";
 import { termContext } from "./Terminal";
@@ -44,10 +45,11 @@ const Output: React.FC<Props> = ({ index, cmd }) => {
           history: <History />,
           projects: <Projects />,
           pwd: <GeneralOutput>/home/LibRComm</GeneralOutput>,
-          socials: <Socials />,
           themes: <Themes />,
           welcome: <Welcome />,
           whoami: <GeneralOutput>visitor</GeneralOutput>,
+          skills: <Skills />,
+          experience: <Experience />,
         }[cmd]
       }
     </OutputContainer>
