b660b48e0f68ba2ec6d4d3ed2eceaa0ca1cafacb
LibRComm <agape@ikmail.com>
2026-05-10 21:01:40 +0200

    Update Projects.tsx

--- src/components/commands/Projects.tsx
+++ src/components/commands/Projects.tsx
@@ -67,14 +67,14 @@ const projects = [
   },
   {
     id: 3,
-    title: "Mise en place et sécurisation d’un outil de supervision réseau (ntopng)",
-    desc: "Dans le cadre de mon homelab, j’ai déployé ntopng afin d’analyser et superviser le trafic réseau en temps réel. J’ai également renforcé la sécurité de l’accès à l’interface en mettant en place un certificat TLS généré avec mkcert. Enfin, j’ai appliqué des bonnes pratiques de sécurisation pour protéger l’outil et limiter les accès non autorisés.",
-    url: "https://github.com/LibRComm/LibRComm.github.io/proxmox-homelab",
+    title: "Centralisation et supervision des logs avec Graylog",
+    desc: "Mise en place d’un serveur Graylog pour centraliser les logs de l’infrastructure virtuelle du homelab. Collecte des événements provenant d'OPNsense et des VM, création de dashboards et expérimentation autour de la détection d’anomalies et de la supervision de sécurité.",
+    url: "https://github.com/LibRComm/LibRComm.github.io-/wiki/%23-Graylog",
   },
   {
     id: 4,
-    title: "Mettre en place un réseau Mesh",
-    desc: "Connecter deux cartes LoRa 32.",
+    title: "Mise en place et sécurisation d’un outil de supervision réseau (ntopng)",
+    desc: "Dans le cadre de mon homelab, j’ai déployé ntopng afin d’analyser et superviser le trafic réseau en temps réel. J’ai également renforcé la sécurité de l’accès à l’interface en mettant en place un certificat TLS généré avec mkcert. Enfin, j’ai appliqué des bonnes pratiques de sécurisation pour protéger l’outil et limiter les accès non autorisés.",
     url: "https://github.com/LibRComm/LibRComm.github.io/proxmox-homelab",
   },
 ];
